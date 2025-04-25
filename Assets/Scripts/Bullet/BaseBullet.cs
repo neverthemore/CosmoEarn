e65e317d6dc7e5225c2fca264e2b96b6a9db8a30
NoPosh <murin8@mail.ru>
2025-04-26 02:39:31 +0300

    Стрельба

--- Assets/Scripts/Bullet/BaseBullet.cs
+++ Assets/Scripts/Bullet/BaseBullet.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -6,14 +7,23 @@ public class BaseBullet : MonoBehaviour
     [SerializeField] float bulletSpeed = 10f;
     [SerializeField] int bulletDamage = 1;
 
+    [SerializeField] Animator[] animator;
+    [SerializeField] GameObject bulletSprite;
+
 
     public bool _isFriendly = false;
+
+    private bool _isExplosed = false;
     //Тут нужно еще делать проверку, своя ли это пуля, чтобы себя не била
     Rigidbody2D rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        foreach (var animator in animator)
+        {
+            animator.gameObject.SetActive(false);
+        }
     }
     public void Attack()
     {
@@ -25,7 +35,7 @@ public class BaseBullet : MonoBehaviour
         {
             rb.linearVelocity = -transform.up * bulletSpeed;
         }
-        Destroy(gameObject, 2f);
+        Destroy(gameObject, 3f);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -34,6 +44,17 @@ public class BaseBullet : MonoBehaviour
         {
             
             collision.gameObject.GetComponent<IDamageable>().TakeDamage(bulletDamage);
+
+            bulletSprite.SetActive(false);
+            gameObject.GetComponent<Collider2D>().enabled = false;
+            rb.linearVelocity = Vector2.zero;
+
+
+            int randomIndex = Random.Range(0, 2);
+
+            animator[randomIndex].gameObject.SetActive(true);
+            animator[randomIndex].SetTrigger("Boom");
+            Destroy(gameObject, 0.5f);
         }
         
     }
